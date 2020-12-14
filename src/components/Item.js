452c24c56c651088dfc5994894303aadc94ccc33
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-14 12:42:36 -0600

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 // We'll need quite a few imports from react-router-dom
-
-import ItemDetails from './ItemDetails'
+import { useParams } from "react-router-dom";
+import ItemDetails from "./ItemDetails";
 
 export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
-  const { items } = props
+  const { items } = props;
 
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
   // we use this hook to grab they dynamic parts of the path (:itemID).
-  const item = {}
+  const item = {};
 
   return (
-    <div className='item-wrapper'>
-      <div className='item-header'>
-        <div className='image-wrapper'>
+    <div className="item-wrapper">
+      <div className="item-header">
+        <div className="image-wrapper">
           <img src={item.imageUrl} alt={item.name} />
         </div>
-        <div className='item-title-wrapper'>
+        <div className="item-title-wrapper">
           <h2>{item.name}</h2>
           <h4>${item.price}</h4>
         </div>
       </div>
 
-      <nav className='item-sub-nav'>
+      <nav className="item-sub-nav">
         {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
       </nav>
 
@@ -34,5 +34,5 @@ export default function Item(props) {
 
       {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
     </div>
-  )
+  );
 }
