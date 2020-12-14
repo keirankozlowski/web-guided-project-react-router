83bbb3b8a7b26ae2c050a0218f208586f64bf4e3
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-14 11:40:32 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
 // Importing the top-level component
-import App from './components/App'
+import App from "./components/App";
 
 // Importing the styles
-import './styles.less'
+import "./styles.less";
 
 // 👉 STEP 1 - Import React Router's Router
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
 render(
   // Wrap the <App /> in a provider
-  <
-  <App />
-  , document.querySelector('#root')
-)
+  <Router>
+    <App />
+  </Router>,
+  document.querySelector("#root")
+);
