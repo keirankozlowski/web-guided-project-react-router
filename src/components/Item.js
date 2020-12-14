62d630c999ad6d8d4705f66c0d31437ab835a0dc
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-14 12:58:38 -0600

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,7 +7,8 @@ export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
   const { items } = props;
   const { url, path } = useRouteMatch();
-  console.log('url', url)
+  console.log("url", url); // makes 
+  console.log("path", path);
 
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
