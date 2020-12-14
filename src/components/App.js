2bd984b6819b355b63f51391adbe84018eaee186
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-14 11:47:27 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch } from "react-router-dom";
 
 // Components used for the different routes
-import Home from './Home'
-import ItemsList from './ItemsList'
-import Item from './Item'
+import Home from "./Home";
+import ItemsList from "./ItemsList";
+import Item from "./Item";
 
 // Dummy data
-import data from '../data'
+import data from "../data";
 
 function fetchStock() {
   // fetchStock simulates getting data through axios.get(<URL>)
-  return Promise.resolve({ success: true, data })
+  return Promise.resolve({ success: true, data });
 }
 
 export default function App(props) {
-  const [stock, setStock] = useState([])
+  const [stock, setStock] = useState([]);
 
   useEffect(() => {
-    fetchStock().then(res => setStock(res.data))
-  }, [])
+    fetchStock().then((res) => setStock(res.data));
+  }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <nav>
-        <h1 className='store-header'>Emily&apos;s Trinkets</h1>
-        <div className='nav-links'>
+        <h1 className="store-header">Emily&apos;s Trinkets</h1>
+        <div className="nav-links">
           {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
-          <a
+          <a href="/">Home</a>
         </div>
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
     </div>
-  )
+  );
 }
